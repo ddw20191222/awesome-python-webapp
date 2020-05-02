d9803d13687faa8cd75dd4db402b2ee2261d86a0
ddw20191222 <15256280211@163.com>
2020-05-02 23:01:42 +0800

    add git_exercise1.py

---
+++ git_exercise1.py
@@ -0,0 +1,8 @@
+#!/d:\\py\\awesome-python-webapp python3
+# -*- coding: utf-8 -*-
+# software: sublime
+# name: git_exercise1.py
+# author: dongdong
+# date: 2020.05.02
+
+this is used to verified that I can push content to github.
